3aa3dfe41e581444621890a47fb4e19f4787af03
IsaacOrtga <ortga.isaac@gmail.com>
2024-06-11 14:36:07 +0200

    initial commit

---
+++ frontend/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { NavbarApp } from './components/pure/navbar/Navbar.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <NavbarApp />
+  </>,
+)
