ac159240dc458bb80f97719e7f3a0ff8167a74cb
IsaacOrtga <ortga.isaac@gmail.com>
2024-06-13 23:58:21 +0200

    updates in form

--- frontend/src/components/pure/form/FormComp.jsx
+++ frontend/src/components/pure/form/FormComp.jsx
@@ -7,10 +7,14 @@ import IconButton from '@mui/material/IconButton';
 import InputAdornment from '@mui/material/InputAdornment';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import Grid from '@mui/material/Grid';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+
 
 export function FormComp({
   formRegisterTitle,
@@ -78,171 +82,169 @@ export function FormComp({
       </div>
       <Form className='formCompRegister-Content' onSubmit={handleSubmit}>
         {showNameDataForm && (
-          <Row>
-            <Col xs={12} md={4} lg={4}>
-              <TextField
-                id="nameForm"
-                label="*Nombre"
-                variant="standard"
-                className='formCompRegister-field'
-                type="text"
-                name='name'
-                value={formData.name}
-                onChange={handleChange} />
-            </Col>
-            <Col xs={12} md={4} lg={4}>
-              <TextField
-                id="lastname_1Form"
-                label="*Primer apellido"
-                variant="standard"
-                className='formCompRegister-field'
-                type="text"
-                name='lastname_1'
-                value={formData.lastname_1}
-                onChange={handleChange} />
-            </Col>
-            <Col xs={12} md={4} lg={4}>
-              <TextField
-                id="lastname_2Form"
-                label="Segundo apellido"
-                variant="standard"
-                className='formCompRegister-field'
-                type="text"
-                name='lastname_2'
-                value={formData.lastname_2}
-                onChange={handleChange} />
-            </Col>
-          </Row>
-        )}
-        <Row>
-          {showDocumentDataForm && (
-            <>
-              <Col xs={3} md={2} lg={2}>
-                <Form.Select aria-label="Choose a type of document" className='formCompRegister-selectDocType' value={selectedDocType} onChange={handleSelectDocType}>
-                  <option value="1">NIF</option>
-                  <option value="2">NIE</option>
-                </Form.Select>
-              </Col>
-              <Col xs={9} md={4} lg={4}>
+          <>
+            <Grid className='formCompRegister-fieldsContent' container columnGap={10} columns={2}>
+              <Grid item>
                 <TextField
-                  id="documentationForm"
-                  label={docType}
+                  id="nameForm"
+                  label="*Nombre"
                   variant="standard"
                   className='formCompRegister-field'
+                  type="text"
+                  name='name'
+                  value={formData.name}
+                  onChange={handleChange} />
+              </Grid>
+              <Grid item>
+                <TextField
+                  id="lastname_1Form"
+                  label="*Primer apellido"
+                  variant="standard"
+                  className='formCompRegister-field'
+                  type="text"
+                  name='lastname_1'
+                  value={formData.lastname_1}
+                  onChange={handleChange} />
+              </Grid>
+            </Grid>
+            <Grid className='formCompRegister-fieldsContent' sx={{ mt: 3 }} container columns={3}>
+              <Grid item>
+                <TextField
+                  id="lastname_2Form"
+                  label="Segundo apellido"
+                  variant="standard"
+                  className='formCompRegister-field formCompRegister-field-lasname_2'
+                  type="text"
+                  name='lastname_2'
+                  value={formData.lastname_2}
+                  onChange={handleChange} />
+              </Grid>
+              <Grid item className='doc-selector' sx={{mt:1}}>
+                <FormControl variant="standard" aria-label="Choose a type of document" className='formCompRegister-selectDocType'>
+                  <InputLabel>*Tipo</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-standard-label"
+                    id="documentSelector"
+                    value={selectedDocType}
+                    onChange={handleSelectDocType}
+                    label={selectedDocType}
+                  >
+                    <MenuItem value="1">NIF</MenuItem>
+                    <MenuItem value="2">NIE</MenuItem>
+                  </Select></FormControl>
+              </Grid>
+              <Grid>
+                <TextField
+                  id="documentationForm"
+                  label={`*${docType}`}
+                  variant="standard"
+                  className='formCompRegister-field formCompRegister-field-documentation'
                   type="text"
                   name='nif'
                   value={formData.nif}
                   onChange={handleChange} />
-              </Col>
-            </>
-          )}
-          {showSpecializationForm && (
-            <Col xs={12} md={6} lg={6}>
-              <Form.Group controlId="specializations" className="formCompRegister-form-floating mb-3 formCompRegister-specializations">
-                <div className="dropdown">
-                  <Dropdown onSelect={handleDropdownChange}>
-                    <Dropdown.Toggle variant="light" id="dropdown-basic" className="w-100 dropdownSpecializations">
-                      {selectedSpecializations.length > 0 ? selectedSpecializations.join(', ') : 'Selecciona especializaciones'}
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu className='menu-specialization'>
-                      <Dropdown.Item className='menuList-spec' eventKey="1">Funky</Dropdown.Item>
-                      <Dropdown.Item className='menuList-spec' eventKey="2">Aéreo</Dropdown.Item>
-                      <Dropdown.Item className='menuList-spec' eventKey="3">Hip-hop</Dropdown.Item>
-                      <Dropdown.Item className='menuList-spec' eventKey="4">Jazz</Dropdown.Item>
-                      <Dropdown.Item className='menuList-spec' eventKey="5">Breakdance</Dropdown.Item>
-                      <Dropdown.Item className='menuList-spec' eventKey="6">Bachata</Dropdown.Item>
-                      <Dropdown.Item className='menuList-spec' eventKey="7">Salsa</Dropdown.Item>
-                      <Dropdown.Item className='menuList-spec' eventKey="8">Merengue</Dropdown.Item>
-                    </Dropdown.Menu>
-                  </Dropdown>
-                </div>
-              </Form.Group>
-            </Col>
-          )}
-        </Row>
-        <Row>
-          {showEmailForm && (
-            <Col xs={12} md={6} lg={6}>
-              <TextField
-                id="emailForm"
-                label="*Email"
-                variant="standard"
-                className='formCompRegister-field'
-                type='email'
-                name='email'
-                value={formData.email}
-                onChange={handleChange} />
-            </Col>
-          )}
-          {showPhoneForm && (
-            <Col xs={12} md={6} lg={6}>
-              <TextField
-                id="phoneForm"
-                label="Teléfono"
-                variant="standard"
-                className='formCompRegister-field'
-                name='phone'
-                value={formData.phone}
-                onChange={handleChange} />
-            </Col>
-          )}
-        </Row>
+              </Grid>
+            </Grid>
+          </>
+        )}
+       <Grid className='formCompRegister-fieldsContent formCompRegister-emailPhoneContent' container columnGap={10} columns={2}>
+        {showEmailForm && (
+          <>
+            <TextField
+              id="emailForm"
+              label="*Email"
+              variant="standard"
+              className='formCompRegister-field'
+              type='email'
+              name='email'
+              value={formData.email}
+              onChange={handleChange} />
+          </>
+        )}
+        {showPhoneForm && (
+          <>
+            <TextField
+              id="phoneForm"
+              label="Teléfono"
+              variant="standard"
+              className='formCompRegister-field'
+              name='phone'
+              value={formData.phone}
+              onChange={handleChange} />
+          </>
+        )}</Grid>
+        <Grid className='formCompRegister-fieldsContent formCompRegister-emailPhoneContent' container columnGap={10} columns={2}>
         {showPassField && (
           <>
-            <Row>
-              <Col>
-                <TextField
-                  id="passwordForm"
-                  label="Contraseña"
-                  variant="standard"
-                  className='formCompRegister-field'
-                  type={passwordField.password.type}
-                  placeholder="Contraseña"
-                  name='password'
-                  value={formData.password}
-                  onChange={handleChange}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton
-                          aria-label="toggle password visibility"
-                          onClick={() => handleShowPassword('password')}
-                          edge="end"
-                        >
-                          {passwordField.password.visible ? <VisibilityIcon /> : <VisibilityOffIcon />}
-                        </IconButton>
-                      </InputAdornment>
-                    ),
-                  }} />
-              </Col>
-              {showPassField_2 && (
-                <Col>
-                  <TextField
-                    id="passwordForm"
-                    label="Repetir contraseña"
-                    variant="standard"
-                    className='formCompRegister-field'
-                    type={passwordField.password_2.type}
-                    name='password_2'
-                    value={formData.password_2}
-                    onChange={handleChange}
-                    InputProps={{
-                      endAdornment: (
-                        <InputAdornment position="end">
-                          <IconButton
-                            aria-label="toggle password visibility"
-                            onClick={() => handleShowPassword('password_2')}
-                            edge="end"
-                          >
-                            {passwordField.password_2.visible ? <VisibilityIcon /> : <VisibilityOffIcon />}
-                          </IconButton>
-                        </InputAdornment>
-                      ),
-                    }} />
-                </Col>
-              )}
-            </Row>
+            <TextField
+              id="passwordForm"
+              label="Contraseña"
+              variant="standard"
+              className='formCompRegister-field'
+              type={passwordField.password.type}
+              placeholder="Contraseña"
+              name='password'
+              value={formData.password}
+              onChange={handleChange}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={() => handleShowPassword('password')}
+                      edge="end"
+                    >
+                      {passwordField.password.visible ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }} />
+            {showPassField_2 && (
+              <TextField
+                id="passwordForm"
+                label="Repetir contraseña"
+                variant="standard"
+                className='formCompRegister-field'
+                type={passwordField.password_2.type}
+                name='password_2'
+                value={formData.password_2}
+                onChange={handleChange}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={() => handleShowPassword('password_2')}
+                        edge="end"
+                      >
+                        {passwordField.password_2.visible ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }} />
+            )}
           </>
+        )}</Grid>
+         {showSpecializationForm && (
+          <FormControl variant='filled' controlId="specializations" className="formCompRegister-form-floating mb-3 formCompRegister-specializations">
+            <div className="dropdown">
+              <Dropdown onSelect={handleDropdownChange}>
+                <Dropdown.Toggle variant="light" id="dropdown-basic" className="w-100 dropdownSpecializations">
+                  {selectedSpecializations.length > 0 ? selectedSpecializations.join(', ') : 'Selecciona especializaciones'}
+                </Dropdown.Toggle>
+                <Dropdown.Menu className='menu-specialization'>
+                  <MenuItem className='menuList-spec' eventKey="1">Funky</MenuItem>
+                  <MenuItem className='menuList-spec' eventKey="2">Aéreo</MenuItem>
+                  <MenuItem className='menuList-spec' eventKey="3">Hip-hop</MenuItem>
+                  <MenuItem className='menuList-spec' eventKey="4">Jazz</MenuItem>
+                  <MenuItem className='menuList-spec' eventKey="5">Breakdance</MenuItem>
+                  <MenuItem className='menuList-spec' eventKey="6">Bachata</MenuItem>
+                  <MenuItem className='menuList-spec' eventKey="7">Salsa</MenuItem>
+                  <MenuItem className='menuList-spec' eventKey="8">Merengue</MenuItem>
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+          </FormControl>
         )}
         {showDropzone && (
           <MyDropzone />
